d6cf5f0b09e10ab7ebb4ef5b090cdb79ff3a090c
Kyle Harding <kylemharding@gmail.com>
2018-01-13 14:39:30 +0000

    install sshd and rsync

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+	
+# create ssh dir if it does not exist
+[ -d "/data/.ssh" ] ||
+	mkdir -p "/data/.ssh"
 
+# touch authorized_keys if it does not exist
+[ -f "/data/.ssh/authorized_keys" ] ||
+	touch "/data/.ssh/authorized_keys"
+
+# set permissions on ssh dir
+chown -R root:root "/data/.ssh"
+chmod -R 700 "/data/.ssh"
+
+# start services
 supervisord -c "/config/supervisord.conf"
