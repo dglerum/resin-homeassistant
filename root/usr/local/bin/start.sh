a6be9d622e7d48fa9e475317c8b21ef2194d7b65
Kyle Harding <kylemharding@gmail.com>
2018-01-28 18:23:56 +0000

    Revert "remove ssh server"
    This reverts commit 36db6293c529eb0f213dc1631b8395599519a0e0.

--- root/usr/local/bin/start.sh
+++ root/usr/local/bin/start.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+	
+# create ssh dir if it does not exist
+if [ ! -d "/data/.ssh" ]
+then
+	mkdir -p "/data/.ssh"
+fi
+
+# touch authorized_keys if it does not exist
+if [ ! -f "/data/.ssh/authorized_keys" ]
+then
+	touch "/data/.ssh/authorized_keys"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "/data/.ssh"
+chmod -R 700 "/data/.ssh"
+
+# generate host keys
+/usr/bin/ssh-keygen -A
 
 # start services
 supervisord -c "/etc/supervisord.conf"
