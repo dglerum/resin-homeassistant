78a574bba760cf192e8867d18e8e7bd45413ffbb
Davy Glerum <dglerum@Davys-MacBook-Pro.local>
2018-09-22 13:04:40 +0200

    little change

---
+++ root/usr/local/bin/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# create ssh dir if it does not exist
+if [ ! -d "/data/.ssh" ]
+then
+	mkdir -p "/data/.ssh"
+fi
+
+if [ ! -d "/data/config" ]
+then
+	mkdir -p "/data/config"
+fi
+
+# touch authorized_keys if it does not exist
+if [ ! -f "/data/.ssh/authorized_keys" ]
+then
+	touch "/data/.ssh/authorized_keys"
+fi
+
+# set permissions on ssh dir
+chown -R root:root "/data/.ssh"
+chmod -R 700 "/data/.ssh"
+
+# generate host keys
+/usr/bin/ssh-keygen -A
+
+# start services
+supervisord -c "/etc/supervisord.conf"
