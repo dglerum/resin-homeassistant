e82abbe13221eff23a734db8a6254fa8d29819f2
Davy Glerum <dglerum@Davys-MacBook-Pro.local>
2018-09-22 15:38:18 +0200

    added mosquitto service

--- mosquitto/root/usr/local/bin/start.sh
+++ mosquitto/root/usr/local/bin/start.sh
@@ -21,7 +21,7 @@ fi
 # touch authorized_keys if it does not exist
 if [ ! -f "/data/mqtt/log/mosquitto.log" ]
 then
-	touch "/data/mqtt/log/mosquitto.log" && chmod o+w "/data/mqtt/log/mosquitto.log"
+	touch "/data/mqtt/log/mosquitto.log" && chmod 777 "/data/mqtt/log/mosquitto.log"
 fi
 
 /usr/sbin/mosquitto -c /data/mqtt/config/mosquitto.conf
