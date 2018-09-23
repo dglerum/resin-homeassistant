eb9174f69a289d309c641468af8b0539fb66fdc7
Davy Glerum <dglerum@Davys-MacBook-Pro.local>
2018-09-23 13:01:13 +0200

    removed mqtt dependancy for hass

--- mosquitto/root/usr/local/bin/start.sh
+++ mosquitto/root/usr/local/bin/start.sh
@@ -3,7 +3,7 @@
 # create ssh dir if it does not exist
 if [ ! -d "/data/mqtt/data" ]
 then
-	mkdir -p "/data/mqtt/data"
+	mkdir -p "/data/mqtt/data" && chmod -R ugo+rw "/data/mqtt/data"
 fi
 
 # create ssh dir if it does not exist
@@ -15,13 +15,13 @@ fi
 # create ssh dir if it does not exist
 if [ ! -d "/data/mqtt/log" ]
 then
-	mkdir -p "/data/mqtt/log"
+	mkdir -p "/data/mqtt/log" && chmod -R ugo+rw "/data/mqtt/log"
 fi
 
 # touch authorized_keys if it does not exist
 if [ ! -f "/data/mqtt/log/mosquitto.log" ]
 then
-	touch "/data/mqtt/log/mosquitto.log" && chmod 777 "/data/mqtt/log/mosquitto.log"
+	touch "/data/mqtt/log/mosquitto.log"
 fi
 
 /usr/sbin/mosquitto -c /data/mqtt/config/mosquitto.conf
