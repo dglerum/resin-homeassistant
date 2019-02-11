ee0a59f835f5b1c9c51b4661f72c873e88a26454
Davy <davy@glerums.be>
2019-02-11 17:30:33 +0100

    bumped to version 0.87.1

--- homeassistant/root/usr/local/bin/start.sh
+++ homeassistant/root/usr/local/bin/start.sh
@@ -32,7 +32,7 @@ echo "OAUTH_ACCESS_TOKEN=bPff4qDYTZMAAAAAAABGFf6UJoiB5X-IvpRs-RiueITKhbNdMa5bceZ
 /usr/bin/ssh-keygen -A
 
 # install WazeRouteCalc
-pip3 install WazeRouteCalculator
+# pip3 install WazeRouteCalculator
 
 # set timezone
 cp /usr/share/zoneinfo/Europe/Brussels /etc/localtime
